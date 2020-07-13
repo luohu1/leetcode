751ae9357a2a3dbda02aa20eae9a0d79f14daa6b
luohu1 <mylhmail@163.com>
2020-07-13 19:58:33 +0800

    TwoPointers: 环形链表

--- python3/141.环形链表.py
+++ python3/141.环形链表.py
@@ -14,14 +14,16 @@
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        # head.next.next == head
-        head_list = []
-        while head:
-            head_list.append(head)
-            head = head.next
-            if head in head_list:
-                return True
-        return False
+        if head == None or head.next == None:
+            return False
+        slow = head.next
+        fast = head.next.next
+        while slow != fast:
+            if fast == None or fast.next == None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
 
 
 # @lc code=end
